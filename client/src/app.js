f93bd5348d95c3b314c9c626251c79e9247dfab2
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-08 16:00:01 -0500

    srcapp

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import './styles/App.css'
+import './styles/style.css'
 import { Routes, Route } from 'react-router-dom'
 import routineList from './components/routineList'
 import routineDetails from './components/routineDetails'
