4b7d67ea44e51962c82a243fcee5eb5f8811f606
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-08 16:34:43 -0500

    seed

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Tabs, Tab } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
 
 export default function Navbar() {
-  const useStyles = makeStyles({
-    nav: {
-      backgroundColor: "red"
-    },
-    link: {
-      color: "white",
-      textDecoration: "none"
-    }
-  });
-
-  const classes = useStyles();
-
   return (
     <AppBar className={classes.nav} position='fixed'>
       <Toolbar>
@@ -24,11 +10,8 @@ export default function Navbar() {
           <Link className={classes.link} to='/'>
             <Tab label="Workouts" />
           </Link>
-          <Link className={classes.link} to='/create'>
+          <Link className={classes.link} to=''>
             <Tab label="Add Workout" />
-          </Link>
-          <Link className={classes.link} to='/user'>
-            <Tab label="Create User" />
           </Link>
         </Tabs>
       </Toolbar>
--- seed/initializeData.js
+++ seed/initializeData.js
@@ -5,17 +5,16 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
   const routine1 = await new Routine({
-    user: ,
+    user: [userName],
     workout: [workout1._id],
+    date: [timeStamp],
   })
   await routine1.save()
 
   const workout1 = await new Workout({
-    user: ,
-    workout: ,
-    duration: ,
-    date: ,
-    description: ,
+    workout: [text],
+    duration: [time],
+    description: [text],
   })
   await workout1.save()
 
