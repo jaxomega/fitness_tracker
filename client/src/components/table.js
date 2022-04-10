18585c2e1b98f9fbc9f84c06e9b42be30ac27b10
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-10 00:24:32 -0500

    tablefile

---
+++ client/src/components/table.js
@@ -0,0 +1,11 @@
+let table = document.createElement('table')
+let thead = document.createElement('thead')
+let tbody = document.createElement('tbody')
+
+table.appendChild(thead)
+table.appendChild(tbody)
+
+document.getElementById('body').appendChild(table)
+
+let row_1 = document.createElement('tr')
+let heading_1 = document.createElement('thead')
