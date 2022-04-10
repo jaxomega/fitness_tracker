d7ab1dc546103ace3eaa3ed457cccb647f1e80fe
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-10 08:17:28 -0500

    appreqres

--- controllers/app.js
+++ controllers/app.js
@@ -1,22 +1,20 @@
-const db = require("../models");
-const Fitness = db.fitness;
+import { mongo } from 'mongoose';
+
+const res = require('../client/src/models');
+const req = require('../client/src/models');
 
 exports.create = (req, res) => {
-  
+    button.onClick("add_row")
 };
 
 exports.findAll = (req, res) => {
-  
+    selectAll("routineTable")
 };
 
 exports.update = (req, res) => {
-  
+    button.onClick("edit_row")
 };
 
 exports.delete = (req, res) => {
-  
-};
-
-exports.findAllPublished = (req, res) => {
-  
+  button.onClick("delete_row")
 };
--- routes/routine_list.js
+++ routes/routine_list.js
@@ -10,133 +10,38 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
-// router.route("/delete/:id").delete((req, res) => {
-//   Routine.findAndDelete(req.params)
-//     .then(() => res.json("Routine Deleted!"))
-//     .catch(err => res.status(400).json(`Error: ${err}`));
-// });
+router.route("/delete/:id").delete((req, res) => {
+  Routine.findAndDelete(req.params)
+    .then(() => res.json("Routine Deleted!"))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
 
-// router.route("/update/:id").post((req, res) => {
-//     Routine.findById(req.params.id)
-//       .then(Routine => {
-//         Routine.workout = req.body.workout;
-//         Routine.duration = Number(req.body.duration);
-//         Routine.date = Date.parse(req.body.date);
-//         Routine.description = req.body.description;
+router.route("/update/:id").post((req, res) => {
+    Routine.findById(req.params.id)
+      .then(Routine => {
+        Routine.workout = req.body.workout;
+        Routine.duration = Number(req.body.duration);
+        Routine.date = Date.parse(req.body.date);
+        Routine.description = req.body.description;
         
-//         Routine
-//           .save()
-//           .then(() => res.json("Routine Updated!"))
-//           .catch(err => res.status(400).json(`Error: ${err}`));
-//       })
-//       .catch(err => res.status(400).json(`Error: ${err}`));
-//   });
+        Routine
+          .save()
+          .then(() => res.json("Routine Updated!"))
+          .catch(err => res.status(400).json(`Error: ${err}`));
+      })
+      .catch(err => res.status(400).json(`Error: ${err}`));
+  });
 
-// router.route('/add').Workout((req, res) => {
-//   const workout = (req.body.workout);
-//   const duration = Number(req.body.duration);
-//   const description = req.body.description;
+router.route('/add').Workout((req, res) => {
+  const workout = (req.body.workout);
+  const duration = Number(req.body.duration);
+  const description = req.body.description;
 
-//   newWorkout
-//     .save()
-//     .then(() => res.json("Workout Added!"))
-//     .catch(err => res.status(400).json(`Error: ${err}`));
-// });
+  newWorkout
+    .save()
+    .then(() => res.json("Workout Added!"))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
 
-
-
-// exports.Routine_create = function (req, res) {
-//   let routine = new Routine(
-//       {
-//         workout: Text(req.body.workout),
-//         duration: Number(req.body.duration),
-//         date: Number(req.body.date),
-//         description: Text(req.body.description)
-//       }
-//   );
-  
-//   routine.save(function (err) {
-//       if (err) {
-//           return next(err);
-//       }
-//       res.send("Routine Added!")
-//   })
-// };
-
-// exports.Routine_details = function (req, res) {
-//   Routine.findById(req.params.id, function (err, Routine) {
-//       if (err) return next(err);
-//       res.send(Routine);
-//   })
-// };
-
-// exports.Routine_update = function (req, res) {
-//   Routine.findByIdAndUpdate(req.params.id, {$set: req.body},
-//     function (err, Routine) {
-//       if (err) return next(err);
-//       res.send("Routine Updated!");
-//     })
-// }
-
-// exports.Routine_delete = function (req, res) {
-//   Routine.findByIdAndRemove(req.params.id, function (err) {
-//       if (err) return next(err);
-//       res.send("Routine Deleted!"+req.params.id)
-//   })
-// };
-
-
-
-function edit_routine(no)
-{
- document.getElementById("edit_button"+no).style.display = "none";
- document.getElementById("save_button"+no).style.display = "block";
-	
- var workout = document.getElementById("workout"+no);
- var duration = document.getElementById("duration"+no);
- var description = document.getElementById("description"+no);
-	
- var workout_data = workout.innerHTML;
- var duration_data = duration.innerHTML;
- var description_data = description.innerHTML;
-	
- workout.innerHTML="<input type='text' id='workout"+no+"' value='"+workout_data+"'>";
- duration.innerHTML="<input type='number' id='duration"+no+"' value='"+duration_data+"'>";
- description.innerHTML="<input type='text' id='description"+no+"' value='"+description_data+"'>";
-}
-
-function save_routine(no)
-{
- var workout_val = document.getElementById("workout_text"+no).value;
- var duration_val = document.getElementById("duration_text"+no).value;
- var description_val = document.getElementById("description_text"+no).value;
-
- document.getElementById("workout_row"+no).innerHTML = workout_val;
- document.getElementById("duration_row"+no).innerHTML = duration_val;
- document.getElementById("description_row"+no).innerHTML = description_val;
-
- document.getElementById("edit_button"+no).style.display = "block";
- document.getElementById("save_button"+no).style.display = "none";
-}
-
-function delete_routine(no)
-{
- document.getElementById("row"+no+"").outerHTML = "";
-}
-
-function add_routine()
-{
- var new_workout = document.getElementById("new_workout").value;
- var new_duration = document.getElementById("new_duration").value;
- var new_description = document.getElementById("new_description").value;
-	
- var table = document.getElementById("routineTable");
- var table_len = (table.rows.length)-1;
- var row = table.insertRow(table_len).outerHTML = "<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
- 
- document.getElementById("new_workout").value = "";
- document.getElementById("new_duration").value = "";
- document.getElementById("new_description").value = "";
-}
 module.exports = router;
 export default workout_list
