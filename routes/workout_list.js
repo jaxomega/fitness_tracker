1623dc2cc701aca3fc6bdcb6a141313fc61ca1a4
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-09 23:14:54 -0500

    newdelete

--- routes/workout_list.js
+++ routes/workout_list.js
@@ -37,6 +37,21 @@ exports.Routine_create = function (req, res) {
       res.send("Routine Added!")
   })
 };
+// router.route("/update/:id").post((req, res) => {
+  //   Routine.findById(req.params.id)
+  //     .then(Routine => {
+  //       Routine.workout = req.body.workout;
+  //       Routine.duration = Number(req.body.duration);
+  //       Routine.date = Date.parse(req.body.date);
+  //       Routine.description = req.body.description;
+        
+  //       Routine
+  //         .save()
+  //         .then(() => res.json("Routine Updated!"))
+  //         .catch(err => res.status(400).json(`Error: ${err}`));
+  //     })
+  //     .catch(err => res.status(400).json(`Error: ${err}`));
+  // });
 
 exports.Routine_details = function (req, res) {
   Routine.findById(req.params.id, function (err, Routine) {
@@ -51,26 +66,18 @@ exports.Routine_update = function (req, res) {
       res.send("Routine Updated!");
     })
 }
+
 // router.route("/delete/:id").delete((req, res) => {
 //   Routine.findAndDelete(req.params)
 //     .then(() => res.json("Routine Deleted!"))
 //     .catch(err => res.status(400).json(`Error: ${err}`));
 // });
 
-router.route("/update/:id").post((req, res) => {
-  Routine.findById(req.params.id)
-    .then(Routine => {
-      Routine.workout = req.body.workout;
-      Routine.duration = Number(req.body.duration);
-      Routine.date = Date.parse(req.body.date);
-      Routine.description = req.body.description;
-      
-      Routine
-        .save()
-        .then(() => res.json("Routine Updated!"))
-        .catch(err => res.status(400).json(`Error: ${err}`));
-    })
-    .catch(err => res.status(400).json(`Error: ${err}`));
-});
+exports.Routine_delete = function (req, res) {
+  Routine.findByIdAndRemove(req.params.id, function (err) {
+      if (err) return next(err);
+      res.send("Routine Deleted!"+req.params.id)
+  })
+};
 
 module.exports = router;
