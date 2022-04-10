28a4921ac858d70df178e8a7856a81d4406a0ba9
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-10 06:09:46 -0500

    conapp

---
+++ controllers/app.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+const Fitness = db.fitness;
+
+exports.create = (req, res) => {
+  
+};
+
+exports.findAll = (req, res) => {
+  
+};
+
+exports.update = (req, res) => {
+  
+};
+
+exports.delete = (req, res) => {
+  
+};
+
+exports.findAllPublished = (req, res) => {
+  
+};
