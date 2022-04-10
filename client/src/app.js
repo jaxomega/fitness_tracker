bf35e56384e835034c71d646e46c3d8e5018ee48
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-10 16:17:40 -0500

    definingapp2

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,20 @@
 import './styles/style.css'
-import Route from 'react'
-import Routes from 'router-dom'
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import CreateRoutine from './components/createRoutine'
 import RoutineList from './components/routineList'
 import RoutineDetails from './components/routineDetails'
+import Table from './components/table'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Fitness Tracker</h1>
       <Routes>
-        <Route path="" element={<CreateRoutine />} />
-        <Route path="" element={<RoutineDetails />} />
-        <Route path="" element={<RoutineList />} />
+        <Route path="create/:id" element={ <CreateRoutine /> } />
+        <Route path="details/:id" element={ <RoutineDetails /> } />
+        <Route path="list/:id" element={ <RoutineList /> } />
+        <Route path="/" element={ <Table /> } />
       </Routes>
     </div>
   )
